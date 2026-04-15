2916f20194516b9228dae39695ac31245dbf5c40
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-04-15 03:32:01 +0300

    Bench: 3553699

--- src/movepick.rs
+++ src/movepick.rs
@@ -236,11 +236,12 @@ impl MovePicker {
             Bitboard(0)
         };
 
+        let in_check = td.board.in_check();
         for entry in self.list.iter_mut() {
             let mv = entry.mv;
             let pt = td.board.piece_on(mv.from()).piece_type();
 
-            entry.score = 2048 * td.quiet_history.get(threats, side, mv) / 1024
+            entry.score = (2048 - 512 * in_check as i32) * td.quiet_history.get(threats, side, mv) / 1024
                 + 1536 * td.conthist(ply, 1, mv) / 1024
                 + td.conthist(ply, 2, mv)
                 + td.conthist(ply, 4, mv)
