cf7fb5c8cb31062630c3684233b555cd9e617678
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-04-21 23:31:35 +0300

    Bench: 3140407

--- src/search.rs
+++ src/search.rs
@@ -598,12 +598,9 @@ fn search<NODE: NodeType>(
     // ProbCut
     let mut probcut_beta = beta + 270 - 75 * improving as i32;
 
-    if cut_node
-        && !is_win(beta)
-        && if is_valid(tt_score) { tt_score >= probcut_beta && !is_decisive(tt_score) } else { eval >= beta }
-        && !tt_move.is_quiet()
-    {
-        let mut move_picker = MovePicker::new_probcut(probcut_beta - eval);
+    let probcut_eval = if is_valid(tt_score) && !is_decisive(tt_score) { tt_score } else { eval };
+    if cut_node && !is_win(beta) && probcut_eval >= probcut_beta && !tt_move.is_quiet() {
+        let mut move_picker = MovePicker::new_probcut(probcut_beta - probcut_eval);
 
         while let Some(mv) = move_picker.next::<NODE>(td, true, ply) {
             if move_picker.stage() == Stage::BadNoisy {
