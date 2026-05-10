a6a93a5c440cb9dc38bc1a174d27ca54f77d980e
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-05-11 00:10:47 +0300

    Bench: 2704806

--- src/movepick.rs
+++ src/movepick.rs
@@ -124,14 +124,34 @@ impl MovePicker {
 
     fn score_noisy(&mut self, td: &ThreadData) {
         let threats = td.board.all_threats();
+        let stm = td.board.side_to_move();
+        let occupancies = td.board.occupancies();
 
         for entry in self.list.iter_mut() {
             let mv = entry.mv;
             let captured = td.board.type_on(mv.capture_sq());
             let pt = td.board.type_on(mv.from());
+            let moving_piece = td.board.moved_piece(mv);
+
+            let capture_history = td.noisy_history.get(threats, moving_piece, mv.to(), captured);
+
+            let recapturers = td.board.attackers_to(mv.to(), occupancies) & td.board.colors(!stm);
+
+            let history_score = if recapturers.is_empty() {
+                capture_history
+            } else {
+                let mut best_recapture_history = i32::MIN;
+
+                for sq in recapturers {
+                    best_recapture_history =
+                        best_recapture_history.max(td.noisy_history.get(threats, td.board.piece_on(sq), mv.to(), pt));
+                }
+
+                capture_history - best_recapture_history
+            };
 
             entry.score = 16 * captured.value()
-                + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured)
+                + history_score
                 + 4000 * (mv.is_promotion() && mv.promo_piece_type() == PieceType::Queen) as i32
                 + (200000 - 20000 * pt as i32) * td.board.in_check() as i32;
         }
