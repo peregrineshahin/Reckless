1b2cb92360577346e6831e3b1d30a54c2d91615f
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-05-20 02:14:02 +0300

    Bench: 2580739

--- src/search.rs
+++ src/search.rs
@@ -522,7 +522,7 @@ fn search<NODE: NodeType>(
             >= beta
                 + (1189 * depth * depth / 128 - 96 * improvement / 1024
                     + 23 * depth
-                    + 600 * correction_value.abs() / 1024
+                    + (600 * correction_value.abs() / 1024) * (raw_eval < beta) as i32 
                     - 60 * (td.board.all_threats() & td.board.colors(stm)).is_empty() as i32
                     - 19)
                     .max(1)
