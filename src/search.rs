c3cad598c806b7f6d54d5cc68565182e1b26e659
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-05-27 19:26:34 +0300

    Bench: 3028705

--- src/search.rs
+++ src/search.rs
@@ -598,8 +598,8 @@ fn search<NODE: NodeType>(
         let mut move_picker = MovePicker::new(Move::NULL, Some(probcut_beta - eval));
 
         while let Some(mv) = move_picker.next::<NODE>(td, true, ply) {
-            if move_picker.stage() == Stage::BadNoisy {
-                break;
+            if move_picker.stage() == Stage::BadNoisy && mv != tt_move {
+                continue;
             }
 
             if mv == td.excluded[ply] {
